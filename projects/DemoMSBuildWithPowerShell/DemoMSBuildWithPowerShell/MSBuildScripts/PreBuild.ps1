60026b226da6d6a485b471279ac22cfafc32a307
Saveen Reddy <saveenr@live.com>
2014-10-12 18:40:32 -0700

    Added a demo of calling powershell from MSBuild

---
+++ projects/DemoMSBuildWithPowerShell/DemoMSBuildWithPowerShell/MSBuildScripts/PreBuild.ps1
@@ -0,0 +1,8 @@
+﻿Set-StrictMode -Version 2
+$ErrorActionPreference = "Stop"
+$scriptfilename = $MyInvocation.MyCommand.Name
+$basename = [System.IO.Path]::GetFileNameWithoutExtension( $scriptfilename )
+$datestring = Get-Date -Format "yyyy_MM_dd_hh_mm_ss"
+$outpath = "D:\"
+$outfilename = Join-Path $outpath ( $basename + $datestring + ".txt")
+"TEST $scriptfilename" | Out-File $outfilename
---
+++ projects/DemoMSBuildWithPowerShell/DemoMSBuildWithPowerShell/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoMSBuildWithPowerShell
+{
+    // http://davidfrette.wordpress.com/2011/01/20/creating-powershell-pre-build-and-post-build-events-for-visual-studio-projects/
+    // http://www.asp.net/web-forms/tutorials/deployment/advanced-enterprise-web-deployment/running-windows-powershell-scripts-from-msbuild-project-files
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
