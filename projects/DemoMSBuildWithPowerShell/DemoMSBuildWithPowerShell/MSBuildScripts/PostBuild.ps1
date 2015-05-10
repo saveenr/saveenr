864e2a0cf666afe3c8c169fa537df8437f3a84a6
Saveen Reddy <saveenr@live.com>
2015-05-10 12:23:01 -0700

    Moved some projects into their own repositories

--- projects/DemoMSBuildWithPowerShell/DemoMSBuildWithPowerShell/MSBuildScripts/PostBuild.ps1
+++
@@ -1,8 +0,0 @@
-﻿Set-StrictMode -Version 2
-$ErrorActionPreference = "Stop"
-$scriptfilename = $MyInvocation.MyCommand.Name
-$basename = [System.IO.Path]::GetFileNameWithoutExtension( $scriptfilename )
-$datestring = Get-Date -Format "yyyy_MM_dd_hh_mm_ss"
-$outpath = "D:\"
-$outfilename = Join-Path $outpath ( $basename + $datestring + ".txt")
-"TEST $scriptfilename" | Out-File $outfilename
