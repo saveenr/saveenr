4d155a247a2025937540bb0ec17bc1569aa0056a
Saveen Reddy <saveenr@live.com>
2015-05-15 16:04:57 -0700

    Added more tools

---
+++ Tools/VisioFontCompare/MainForm.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace VisioFontCompare
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonRun = new System.Windows.Forms.Button();
+            this.comboBoxFont1 = new System.Windows.Forms.ComboBox();
+            this.comboBoxFont2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBoxFont1Bold = new System.Windows.Forms.CheckBox();
+            this.checkBoxFont1Italic = new System.Windows.Forms.CheckBox();
+            this.checkBoxFont2Italic = new System.Windows.Forms.CheckBox();
+            this.checkBoxFont2Bold = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // buttonRun
+            // 
+            this.buttonRun.Location = new System.Drawing.Point(303, 226);
+            this.buttonRun.Name = "buttonRun";
+            this.buttonRun.Size = new System.Drawing.Size(75, 23);
+            this.buttonRun.TabIndex = 0;
+            this.buttonRun.Text = "Run";
+            this.buttonRun.UseVisualStyleBackColor = true;
+            this.buttonRun.Click += new System.EventHandler(this.buttonRun_Click);
+            // 
+            // comboBoxFont1
+            // 
+            this.comboBoxFont1.FormattingEnabled = true;
+            this.comboBoxFont1.Location = new System.Drawing.Point(12, 29);
+            this.comboBoxFont1.Name = "comboBoxFont1";
+            this.comboBoxFont1.Size = new System.Drawing.Size(366, 21);
+            this.comboBoxFont1.TabIndex = 1;
+            // 
+            // comboBoxFont2
+            // 
+            this.comboBoxFont2.FormattingEnabled = true;
+            this.comboBoxFont2.Location = new System.Drawing.Point(12, 166);
+            this.comboBoxFont2.Name = "comboBoxFont2";
+            this.comboBoxFont2.Size = new System.Drawing.Size(366, 21);
+            this.comboBoxFont2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "First Font";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Second Font";
+            // 
+            // checkBoxFont1Bold
+            // 
+            this.checkBoxFont1Bold.AutoSize = true;
+            this.checkBoxFont1Bold.Location = new System.Drawing.Point(16, 57);
+            this.checkBoxFont1Bold.Name = "checkBoxFont1Bold";
+            this.checkBoxFont1Bold.Size = new System.Drawing.Size(47, 17);
+            this.checkBoxFont1Bold.TabIndex = 5;
+            this.checkBoxFont1Bold.Text = "Bold";
+            this.checkBoxFont1Bold.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxFont1Italic
+            // 
+            this.checkBoxFont1Italic.AutoSize = true;
+            this.checkBoxFont1Italic.Location = new System.Drawing.Point(16, 80);
+            this.checkBoxFont1Italic.Name = "checkBoxFont1Italic";
+            this.checkBoxFont1Italic.Size = new System.Drawing.Size(48, 17);
+            this.checkBoxFont1Italic.TabIndex = 6;
+            this.checkBoxFont1Italic.Text = "Italic";
+            this.checkBoxFont1Italic.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxFont2Italic
+            // 
+            this.checkBoxFont2Italic.AutoSize = true;
+            this.checkBoxFont2Italic.Location = new System.Drawing.Point(16, 216);
+            this.checkBoxFont2Italic.Name = "checkBoxFont2Italic";
+            this.checkBoxFont2Italic.Size = new System.Drawing.Size(48, 17);
+            this.checkBoxFont2Italic.TabIndex = 8;
+            this.checkBoxFont2Italic.Text = "Italic";
+            this.checkBoxFont2Italic.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxFont2Bold
+            // 
+            this.checkBoxFont2Bold.AutoSize = true;
+            this.checkBoxFont2Bold.Location = new System.Drawing.Point(16, 193);
+            this.checkBoxFont2Bold.Name = "checkBoxFont2Bold";
+            this.checkBoxFont2Bold.Size = new System.Drawing.Size(47, 17);
+            this.checkBoxFont2Bold.TabIndex = 7;
+            this.checkBoxFont2Bold.Text = "Bold";
+            this.checkBoxFont2Bold.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(389, 307);
+            this.Controls.Add(this.checkBoxFont2Italic);
+            this.Controls.Add(this.checkBoxFont2Bold);
+            this.Controls.Add(this.checkBoxFont1Italic);
+            this.Controls.Add(this.checkBoxFont1Bold);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxFont2);
+            this.Controls.Add(this.comboBoxFont1);
+            this.Controls.Add(this.buttonRun);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "MainForm";
+            this.Text = "VisioFontCompare";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonRun;
+        private System.Windows.Forms.ComboBox comboBoxFont1;
+        private System.Windows.Forms.ComboBox comboBoxFont2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBoxFont1Bold;
+        private System.Windows.Forms.CheckBox checkBoxFont1Italic;
+        private System.Windows.Forms.CheckBox checkBoxFont2Italic;
+        private System.Windows.Forms.CheckBox checkBoxFont2Bold;
+    }
+}
+
---
+++ Tools/VisioFontCompare/MainForm.cs
@@ -0,0 +1,459 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using IVisio = Microsoft.Office.Interop.Visio;
+using VA = VisioAutomation;
+using VisioAutomation.Extensions;
+using Application = Microsoft.Office.Interop.Visio.Application;
+
+namespace VisioFontCompare
+{
+    public partial class MainForm : Form
+    {
+        private static Application _app;
+        private List<string> allfontnames;
+
+
+        string text1 =
+    @"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvqxyz1234567890!@#$%^&*()``_-+=[]{}\|;:'"",.<>/?";
+        string text2 =
+            @"When from behind that craggy steep till then The horizon’s bound, a huge peak, black and huge, As if with voluntary power instinct, Upreared its head. I struck and struck again, And growing still in stature the grim shape Towered up between me and the stars, and still, for so it seemed with purpose of its own And measured motion like a living thing, Strode after me.
+
+-William Wordsworth 
+The Prelude, lines 381-389";
+
+        string text3 =
+            @"int function_x( int[] a)
+{
+    string s = ""hello"";
+    char c = s[3];
+    var o = new SomeObject();
+    return 27 * 2;
+}
+";
+
+        string text4 =
+            @"<html>
+    <head>
+        <title>Untitled</title>
+    <head>
+    <body>
+        <p>Hello World</p>
+    </body>
+</html>
+";
+
+        string stencilname = "basic_u.vss";
+        string rectmaster = "Rectangle";
+
+        private VA.Text.CharStyle font1charstyle;
+        private VA.Text.CharStyle font2charstyle;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            _app = new IVisio.Application();
+            var docs = _app.Documents;
+            var doc = docs.Add("");
+            this.allfontnames = doc.Fonts.AsEnumerable().Select(f => f.Name).OrderBy( s=>s).ToList();
+
+            this.comboBoxFont1.DataSource = allfontnames.ToList();
+            this.comboBoxFont2.DataSource = allfontnames.ToList();
+
+            this.comboBoxFont1.Text = "Arial";
+            this.comboBoxFont2.Text = "Calibri";
+
+
+        }
+
+        void FontCompare()
+        {
+            this.font1charstyle = VA.Text.CharStyle.None;
+            if (this.checkBoxFont1Bold.Checked)
+            {
+                this.font1charstyle = this.font1charstyle | VA.Text.CharStyle.Bold;
+            }
+            if (this.checkBoxFont1Italic.Checked)
+            {
+                this.font1charstyle = this.font1charstyle | VA.Text.CharStyle.Italic;
+            }
+
+            this.font2charstyle = VA.Text.CharStyle.None;
+            if (this.checkBoxFont2Bold.Checked)
+            {
+                this.font2charstyle = this.font2charstyle | VA.Text.CharStyle.Bold;
+            }
+            if (this.checkBoxFont2Italic.Checked)
+            {
+                this.font2charstyle = this.font2charstyle | VA.Text.CharStyle.Italic;
+            }
+
+
+
+            var labelfont = "Segoe UI Light";
+
+            var fci = new FontCompareInput();
+
+            var docs = _app.Documents;
+            fci.Document = docs.Add("");
+            var dfonts = fci.Document.Fonts;
+            fci.TargetFontNames = new List<string> { this.comboBoxFont1.Text, this.comboBoxFont2.Text };
+            fci.Styles = new List<VA.Text.CharStyle>();
+            fci.Styles.Add(this.font1charstyle);
+            fci.Styles.Add(this.font2charstyle);
+
+
+            fci.FontSizeFormulas = new List<string> { "8pt", "10pt", "12pt", "14pt", "18pt", "28pt" };
+            fci.TextBlocks = new[] { text1, text2, text3, text4 };
+            fci.LabelFontID = dfonts[labelfont].ID;
+            fci.TargetFontIDs = fci.TargetFontNames.Select(f => dfonts[f].ID).ToList();
+
+            fci.TargetFontDisplayNames = new List<string>();
+
+            for (int i = 0; i < fci.TargetFontNames.Count; i++)
+            {
+                if (fci.Styles[i]!=VA.Text.CharStyle.None)
+                {
+                    var sb = new System.Text.StringBuilder();
+                    sb.Append(fci.TargetFontNames[i]);
+                    sb.Append(" (");
+                    var tokens = new List<string>();
+                    if ((fci.Styles[i] & VA.Text.CharStyle.Bold)!=0)
+                    {
+                        tokens.Add("bold");
+                    }
+                    if ((fci.Styles[i] & VA.Text.CharStyle.Italic)!=0)
+                    {
+                        tokens.Add("italic");
+                    }
+                    sb.Append(string.Join(",", tokens));
+                    sb.Append(")");
+                    fci.TargetFontDisplayNames.Add(sb.ToString());
+
+                }
+                else
+                {
+                    fci.TargetFontDisplayNames.Add(fci.TargetFontNames[i]);
+                }
+            }
+            var first_page = fci.Document.Pages[1];
+            compate_text_blocks(fci);
+            compare_glyph_chart(fci);
+            compare_glyphs(fci);
+            first_page.Delete(1);
+        }
+
+        void compate_text_blocks(FontCompareInput fci)
+        {
+            double left = 1;
+            double vs = 0.25;
+            double cell1_w = 1.0;
+            double cell1_h = 2.0;
+            double cell1_top = 8.5;
+            double cell2_h = 0.5;
+            double cell2_w = 8.0;
+            double cell_sep = 0.5;
+            string labeltextcolor = "rgb(0,176,240)";
+
+            var char1 = new VA.Text.CharacterFormatCells();
+            char1.Font = fci.LabelFontID;
+            char1.Size = "30pt";
+            char1.Color = labeltextcolor;
+
+            var char2 = new VA.Text.CharacterFormatCells();
+            char2.Font = fci.LabelFontID;
+            char2.Size = "16pt";
+            char2.Color = labeltextcolor;
+
+            var fmt1 = new VA.Format.ShapeFormatCells();
+            fmt1.LineWeight = 0;
+            fmt1.LinePattern = 0;
+            fmt1.FillPattern = 0;
+
+            var fmt2 = new VA.Format.ShapeFormatCells();
+            fmt2.LineWeight = 0;
+            fmt2.LinePattern = 0;
+            fmt2.FillPattern = 0;
+
+            var fmt3 = new VA.Format.ShapeFormatCells();
+            fmt3.LineWeight = 0;
+            fmt3.LinePattern = 0;
+            fmt3.FillPattern = 0;
+
+            var tb1 = new VA.Text.TextBlockFormatCells();
+            tb1.VerticalAlign = 0;
+
+            var para1 = new VA.Text.ParagraphFormatCells();
+            para1.HorizontalAlign = 2;
+
+            var para2 = new VA.Text.ParagraphFormatCells();
+            para2.HorizontalAlign = 0;
+
+            var char3 = new VA.Text.CharacterFormatCells();
+
+            var para3 = new VA.Text.ParagraphFormatCells();
+            para3.HorizontalAlign = 0;
+
+            var tb3 = new VA.Text.TextBlockFormatCells();
+            tb3.VerticalAlign = 0;
+
+            foreach (string text in fci.TextBlocks)
+            {
+                var curpage = fci.Document.Pages.Add();
+                foreach (var size in fci.FontSizeFormulas)
+                {
+                    var shape1 = curpage.DrawRectangle(left, cell1_top - cell1_h, left + cell1_w, cell1_top);
+                    shape1.Text = string.Format("{0}", size);
+
+                    var update1 = new VA.ShapeSheet.Update.SRCUpdate();
+                    para1.Apply(update1, 0);
+                    tb1.Apply(update1);
+                    char1.Apply(update1, 0);
+                    fmt1.Apply(update1);
+                    update1.Execute(shape1);
+
+                    double cell2_top = cell1_top;
+                    for (int i = 0; i < fci.TargetFontNames.Count(); i++)
+                    {
+                        double cell2_bottom = cell2_top - cell2_h;
+                        var fontname = fci.TargetFontDisplayNames[i];
+                        double cell2_left = left + cell1_w + cell_sep;
+                        var shape2 = curpage.DrawRectangle(cell2_left, cell2_bottom, cell2_left + cell2_w, cell2_top);
+                        shape2.Text = string.Format("{0}", fontname);
+
+                        double cell3_h = 2.0; // default height
+                        var cell3_top = cell2_bottom;
+                        var cell3_bottom = cell3_top - cell3_h;
+
+                        var shape_3 = curpage.DrawRectangle(cell2_left, cell3_bottom, cell2_left + cell2_w, cell3_top);
+                        shape_3.Text = text;
+
+
+                        char3.Font = fci.TargetFontIDs[i];
+                        char3.Size = size;
+                        char3.Style = (int) (fci.Styles[i]);
+
+                        var update3 = new VA.ShapeSheet.Update.SRCUpdate();
+                        para3.Apply(update3, 0);
+                        tb3.Apply(update3);
+                        char3.Apply(update3, 0);
+                        fmt3.Apply(update3);
+                        update3.Execute(shape_3);
+
+
+                        char2.Font = fci.TargetFontIDs[i];
+                        var update2 = new VA.ShapeSheet.Update.SRCUpdate();
+                        para2.Apply(update2, 0);
+                        //tb1.Apply(update2);
+                        char2.Apply(update2, 0);
+                        fmt2.Apply(update2);
+                        update2.Execute(shape2);
+
+
+                        shape_3.CellsU["Height"].FormulaU = "TEXTHEIGHT(TheText,TxtWidth)";
+                        var cell3_real_size = new VA.Drawing.Size(shape_3.CellsU["Width"].get_Result(null),
+                                                                  shape_3.CellsU["Height"].get_Result(null));
+                        shape_3.CellsU["PinY"].FormulaU = (cell2_bottom - (cell3_real_size.Height / 2.0)).ToString();
+
+                        cell2_top -= cell2_h + cell3_real_size.Height + vs;
+                    }
+                    cell1_top = cell2_top;
+                }
+
+                curpage.ResizeToFitContents(1.0, 1.0);
+            }
+        }
+
+        void compare_glyph_chart(FontCompareInput fci)
+        {
+            var text =
+                "ABCDEFGHIJKLMNOPQRSTUVWXYZ\nabcdefghijklmnopqrstuvqxyz\n1234567890\n!@#$%^&*()\n`~_-+=[]{}<>\n\\|;:'\",./?";
+            var text1_x_lines = text.Split(new char[] { '\n' });
+
+            var page = fci.Document.Pages.Add();
+            var dom = new VA.DOM.Document();
+
+            double cy = 8.0;
+            for (int fi = 0; fi < fci.TargetFontNames.Count; fi++)
+            {
+                var font = fci.TargetFontNames[fi];
+
+                var trect = new VA.Drawing.Rectangle(0, cy - 1.0, 10, cy);
+                var tshape = dom.Drop(rectmaster,stencilname,trect);
+                tshape.Text = new VA.Text.Markup.TextElement(font);
+                tshape.ShapeCells.FillPattern = 0;
+                tshape.ShapeCells.HAlign = 0;
+                tshape.CharFontName = font;
+                tshape.ShapeCells.CharSize = "36pt";
+                tshape.ShapeCells.LinePattern = 0;
+                tshape.ShapeCells.LineWeight = 0;
+                cy -= 2.0;
+
+                for (int ri = 0; ri < text1_x_lines.Length; ri++)
+                {
+                    var curline = text1_x_lines[ri];
+                    for (int c = 0; c < curline.Length; c++)
+                    {
+                        double x = 0 + (1.0) * c;
+                        var rect = new VA.Drawing.Rectangle(x, cy, x + 0.5, cy + 0.5);
+
+                        var shape = dom.Drop(rectmaster, stencilname,rect);
+                        shape.Text = new VA.Text.Markup.TextElement(curline[c].ToString());
+                        shape.ShapeCells.FillPattern = 0;
+                        shape.ShapeCells.CharSize = "18pt";
+                        shape.ShapeCells.LineColor = "rgb(230,230,230)";
+                        shape.ShapeCells.CharStyle = (int) (fci.Styles[fi]);
+                    }
+                    cy -= 1.0;
+                }
+            }
+            dom.Render(page);
+            page.ResizeToFitContents(1.0, 1.0);
+        }
+
+        private void compare_glyphs(FontCompareInput fci)
+        {
+            var colorints = new[]
+                             {
+                                    0x00B0F0, 0xff0000, 0x00B050
+                             };
+
+
+            var colors = colorints.Select(i => new VA.Drawing.ColorRGB(i)).Select(c => c.ToFormula()).ToArray();
+
+            var text =
+                "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvqxyz1234567890«»ßäüöĆćŚśŹźŻżĄąĘę!@#$%^&*()`~_-+=[]{}\\|;:'\",.<>/?";
+            var texts = Split(text, 10);
+
+            var pages = fci.Document.Pages;
+            double col_width = 3.0;
+            double label_height = 0.5;
+            double sample_height = col_width;
+
+            for (int i = 0; i < texts.Count; i++)
+            {
+                var page = pages.Add();
+                var dom = new VA.DOM.Document();
+
+                double cy = 8.0;
+                for (int j = 0; j < texts[i].Count; j++)
+                {
+                    double title_bottom = cy - label_height;
+                    for (int k = 0; k < fci.TargetFontNames.Count(); k++)
+                    {
+                        double left = 0 + (k * col_width);
+                        double right = left + col_width;
+                        double top = cy;
+                        var rect = new VA.Drawing.Rectangle(left, title_bottom, right, top);
+                        var char_sample_title_shape = dom.Drop(rectmaster,stencilname, rect);
+                        char_sample_title_shape.Text = new VA.Text.Markup.TextElement(fci.TargetFontDisplayNames[k]);
+                        char_sample_title_shape.ShapeCells.LinePattern = 0;
+                        char_sample_title_shape.ShapeCells.LineWeight = 0;
+                        char_sample_title_shape.ShapeCells.CharFont = fci.TargetFontIDs[k];
+                        char_sample_title_shape.ShapeCells.FillPattern = 0;
+
+                    }
+                    double overlay_title_left = fci.TargetFontNames.Count()*col_width;
+
+                    double overlay_title_right = overlay_title_left + col_width;
+                    var rectangle = new VA.Drawing.Rectangle(overlay_title_left, title_bottom, overlay_title_right, cy);
+                    var overlay_title_shape = dom.Drop(rectmaster,stencilname,rectangle);
+                    overlay_title_shape.ShapeCells.FillPattern = 0;
+                    overlay_title_shape.ShapeCells.LinePattern = 0;
+                    overlay_title_shape.ShapeCells.LineWeight = 0;
+                    overlay_title_shape.Text = new VA.Text.Markup.TextElement();
+                    overlay_title_shape.ShapeCells.CharTransparency = "0.3";
+                    for (int k = 0; k < fci.TargetFontNames.Count(); k++)
+                    {
+                        var el = overlay_title_shape.Text.AppendElement(fci.TargetFontDisplayNames[k] + "\n");
+                        el.CharacterFormat.Color = new VA.Drawing.ColorRGB(colorints[k % colorints.Count()]);
+                        el.CharacterFormat.FontID = fci.TargetFontIDs[k];
+                    }
+                    cy -= label_height;
+
+                    var cur_char = texts[i][j].ToString();
+                    var bigcharsize = "160pt";
+                    for (int k = 0; k < fci.TargetFontNames.Count(); k++)
+                    {
+                        double w = col_width;
+                        double x0 = 0 + (k * w);
+                        double x1 = x0 + w;
+                        double y0 = cy - sample_height;
+                        double y1 = cy;
+                        var char_sample_shape = dom.Drop(rectmaster,stencilname,new VA.Drawing.Rectangle(x0, y0, x1, y1));
+                        char_sample_shape.Text = new VA.Text.Markup.TextElement(cur_char);
+                        char_sample_shape.ShapeCells.LinePattern = 0;
+                        char_sample_shape.ShapeCells.LineWeight = 0;
+                        char_sample_shape.ShapeCells.CharSize = bigcharsize;
+                        char_sample_shape.ShapeCells.CharFont = fci.TargetFontIDs[k];
+                        char_sample_shape.ShapeCells.FillPattern = 0;
+                        char_sample_shape.ShapeCells.CharStyle = ((int) fci.Styles[k]).ToString();
+                    }
+
+                    for (int k = 0; k < fci.TargetFontNames.Count(); k++)
+                    {
+                        double overlay_left = fci.TargetFontNames.Count()*col_width;
+                        double overlay_right = overlay_left + col_width;
+                        var rect = new VA.Drawing.Rectangle(overlay_left, cy - col_width, overlay_right, cy);
+                        var overlay_shape = dom.Drop(rectmaster,stencilname,rect);
+                        overlay_shape.ShapeCells.LinePattern = 0;
+                        overlay_shape.ShapeCells.LineWeight = 0;
+                        overlay_shape.Text = new VA.Text.Markup.TextElement(cur_char);
+                        overlay_shape.ShapeCells.LinePattern = 0;
+                        overlay_shape.ShapeCells.LineWeight = 0;
+                        overlay_shape.ShapeCells.CharSize = bigcharsize;
+                        overlay_shape.ShapeCells.CharFont = fci.TargetFontIDs[k];
+                        overlay_shape.ShapeCells.FillPattern = 0;
+                        overlay_shape.ShapeCells.CharTransparency = "0.7";
+                        overlay_shape.ShapeCells.CharColor = colors[k % colors.Count()];
+                        overlay_shape.ShapeCells.CharStyle = ((int)fci.Styles[k]).ToString();
+                    }
+
+                    cy -= sample_height;
+
+                    cy -= 1.0; // extra spacing
+                }
+                dom.Render(page);
+                page.ResizeToFitContents(1.0, 1.0);
+
+            }
+        }
+
+        public static List<List<T>> Split<T>(IEnumerable<T> source, int n)
+        {
+            return source
+                .Select((x, i) => new { Index = i, Value = x })
+                .GroupBy(x => x.Index / n)
+                .Select(x => x.Select(v => v.Value).ToList())
+                .ToList();
+        }
+
+        private void buttonRun_Click(object sender, EventArgs e)
+        {
+            FontCompare();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+
+    public class FontCompareInput
+    {
+        public List<string> TargetFontNames;
+        public IVisio.Document Document;
+        public string[] TextBlocks;
+        public List<string> FontSizeFormulas;
+        public List<int> TargetFontIDs;
+        public int LabelFontID;
+        public List<string> TargetFontDisplayNames;
+        public List<VA.Text.CharStyle> Styles;
+    }
+}
